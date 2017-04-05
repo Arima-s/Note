55539bdbb926de7bdfa39922b54162911add483f
Niklaus <monkeydlufyandy@gmail.com>
2017-04-05 18:05:39 +0800

    Update Unique_Array.js

--- Unique_Array.js
+++ Unique_Array.js
@@ -1,7 +1,7 @@
 //  indexOf   不兼容 IE6-8
 function unique(arr) {
 	var _arr = [],
-			arrLength = arr.length;
+	    arrLength = arr.length;
 	for(var i = 0; i < arrLength; i++) {
 		var item = arr[i];
 		if(_arr.indexOf(item) === -1){
